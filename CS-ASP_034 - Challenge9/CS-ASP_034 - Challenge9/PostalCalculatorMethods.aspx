60807cc14b17088f06952eafe9acdb4e395d53f5
obiwannab <obiwannab@comcast.net>
2015-07-14 18:22:46 -0700

    Postage Calculator

---
+++ CS-ASP_034 - Challenge9/CS-ASP_034 - Challenge9/PostalCalculatorMethods.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PostalCalculatorMethods.aspx.cs" Inherits="CS_ASP_034___Challenge9.PostalCalculatorMethods" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Length:&nbsp;
+        <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+&nbsp;
+        <asp:Label ID="lengthFeedback" runat="server" Font-Italic="True" ForeColor="Red" Text="Required" Visible="False"></asp:Label>
+        <br />
+        Width:&nbsp;
+        <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+&nbsp;
+        <asp:Label ID="widthFeedback" runat="server" Font-Italic="True" ForeColor="Red" Text="Required" Visible="False"></asp:Label>
+        <br />
+        Height:&nbsp;
+        <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingGroup" Text="Ground" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="shippingFeedback" runat="server" Font-Bold="True" Font-Italic="False" ForeColor="Blue" Text="Please choose a shipping method"></asp:Label>
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingGroup" Text="Air" />
+        <br />
+        <asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingGroup" Text="Next Day" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_034 - Challenge9/CS-ASP_034 - Challenge9/PostalCalculatorMethods.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_034___Challenge9
+{
+    public partial class PostalCalculatorMethods : System.Web.UI.Page
+    {
+        //Initialize Feedback Fields
+        /* no parameters
+             * no return */
+        private void initializeFeedback()
+            {
+                widthFeedback.Visible = false;
+                lengthFeedback.Visible = false;
+                shippingFeedback.Visible = true;
+                resultLabel.Text = "";
+            }
+
+        //Check for width
+        /* no parameters
+             * Return: boolean */
+        private bool widthEntered()
+            {
+                if(widthTextBox.Text == "")
+                {
+                    widthFeedback.Visible = true;
+                    return false;
+                }
+                else
+                {
+                    widthFeedback.Visible = false;
+                    return true;
+                }
+            }
+
+        //Check for length
+        /* no parameters
+             * Return: boolean */
+        private bool lengthEntered()
+            {
+                if(lengthTextBox.Text == "")
+                {
+                    lengthFeedback.Visible = true;
+                    return false;
+                }
+                else
+                {
+                    lengthFeedback.Visible = false;
+                    return true;
+                }
+            }
+        
+        //Get shipping multiplier based on user selection
+        /* no parameters
+         * Return: bool
+         * Out: double shippingMultiplier */
+        private bool getShippingMultiplier(out double multiplier)
+        {
+            multiplier = 0.0;
+            if (groundRadioButton.Checked) 
+            { 
+                multiplier = .15;
+                shippingFeedback.Visible = false;
+                return true; 
+            }
+            else if (airRadioButton.Checked) 
+            { 
+                multiplier = .25;
+                shippingFeedback.Visible = false;
+                return true; 
+            }
+            else if (nextDayRadioButton.Checked) 
+            { 
+                multiplier = .45;
+                shippingFeedback.Visible = false;
+                return true; 
+            }
+            else 
+            { 
+                shippingFeedback.Visible = true; 
+                return false; 
+            }
+        }
+
+        //Calculate postage
+        /* Parameters:  double shippingMultiplier
+             *              double length required
+             *              double width required
+             *              double height optional
+             * Return: double postage*/
+        private double calculatePostage(double shippingMultiplier, double length, double width, double height = 1)
+            {
+                double volume = length * width * height;
+                double postage = volume * shippingMultiplier;
+                return postage;
+            }
+
+        //Display shipping results
+        /* Parameters:  double postage
+             * no return */
+        private void displayShipping(double postage)
+            {
+                resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", postage);
+            }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //Initialize Feedback
+                initializeFeedback();
+            }
+
+            //Identify shipping multiplier and initialize variables
+            double postage = 0.0;
+            double shippingMultiplier = 0.0;
+            if (getShippingMultiplier(out shippingMultiplier))
+            {
+                //Check for required information
+                if (lengthEntered() && widthEntered())
+                {
+                    double length = double.Parse(lengthTextBox.Text);
+                    double width = double.Parse(widthTextBox.Text);
+
+                    //Check for optional information
+                    if (heightTextBox.Text == "")
+                    {
+                        postage = calculatePostage(shippingMultiplier, length, width);
+                    }
+                    else
+                    {
+                        double height = double.Parse(heightTextBox.Text);
+                        postage = calculatePostage(shippingMultiplier, length, width, height);
+                    }
+
+                    //Display postage to user
+                    displayShipping(postage);
+                }
+                else resultLabel.Text = "";
+            }
+        }
+    }
+}
